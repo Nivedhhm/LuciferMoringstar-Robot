2f343d446c47b2c92ea5071afd640d883c74c5c5
Nivedhhm <89673791+Nivedhhm@users.noreply.github.com>
2022-03-19 12:21:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /DarkSnyder-Robot
 fi
-cd /LuciferMoringstar-Robot
+cd /DarkSnyder-Robot
 pip3 install -U -r requirements.txt
 echo "Starting DarkSnyder RoBot...."
 python3 main.py
